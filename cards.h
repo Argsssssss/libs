aa4c72f19305f78485fb78a058aacb32d6d3ec3f
Cryptofun.jr <61570493+Argsssssss@users.noreply.github.com>
2026-04-16 10:32:46 +0700

    cards.h

--- cards.h
+++ cards.h
@@ -6,6 +6,7 @@ struct Card
     int card;
     int suits;
 };
+// print_C(user_card_1.card_on_hands_1.card = 2 + rand() % (15 - 2 + 1) + 2, user_card_1.card_on_hands_1.suits = rand() % 4);
 string card_collection(int card){
     string over_char[] = {"10", "J", "Q", "K", "A"};
     if(card > 9){
@@ -13,7 +14,7 @@ string card_collection(int card){
     }else{
         return to_string(card);
     }
-    return "QWE";
+    return "ERROR";
 }
 void print_C(Card card_1, Card card_2){
     
@@ -31,22 +32,25 @@ void print_C(Card card_1, Card card_2){
 
     cout << "\n╚═════╝" << "  " << "╚═════╝";
 }
-void print_C(Card card_1, Card card_2, Card card_3, Card card_4){
+void print_C(Card card_1, Card card_2, Card card_3, Card card_4, Card card_5){
 
-    cout << "╔═════╗" << "  " << "╔═════╗" << "  " << "╔═════╗" << "  " << "╔═════╗\n";
-    
+    cout << "╔═════╗" << "  " << "╔═════╗" << "  " << "╔═════╗" << "  " << "╔═════╗" << "  " << "╔═════╗\n";
+
     cout << "║" << card_collection(card_1.card);
     if(card_1.card == 10){cout <<"  ";} else{cout <<"   ";} cout << base_cards[card_1.suits % 4] <<"║  ║" << card_collection(card_2.card);
     if(card_2.card == 10){cout <<"  ";} else{cout <<"   ";} cout << base_cards[card_2.suits % 4] << "║  ║" << card_collection(card_3.card);
     if(card_3.card == 10){cout <<"  ";} else{cout <<"   ";} cout << base_cards[card_3.suits % 4] <<"║  ║" << card_collection(card_4.card);
-    if(card_4.card == 10){cout <<"  ";} else{cout <<"   ";} cout << base_cards[card_4.suits % 4] << "║" << endl;
-    cout << "║     ║  ║     ║  ║     ║  ║     ║\n";
+    if(card_4.card == 10){cout <<"  ";} else{cout <<"   ";} cout << base_cards[card_4.suits % 4] << "║  ║" << card_collection(card_5.card);
+    if(card_5.card == 10){cout <<"  ";} else{cout <<"   ";} cout << base_cards[card_5.suits % 4] << "║" << endl; 
+    cout << "║     ║  ║     ║  ║     ║  ║     ║  ║     ║\n";
     cout << "║" << base_cards[card_1.suits % 4];
     if(card_1.card == 10){cout <<"  ";} else{cout <<"   ";} cout << card_collection(card_1.card)  <<"║  ║" << base_cards[card_2.suits % 4];
     if(card_2.card == 10){cout <<"  ";} else{cout <<"   ";} cout <<  card_collection(card_2.card) << "║  ║" << base_cards[card_3.suits % 4];
     if(card_3.card == 10){cout <<"  ";} else{cout <<"   ";} cout << card_collection(card_3.card) <<"║  ║" << base_cards[card_4.suits % 4];
-    if(card_4.card == 10){cout <<"  ";} else{cout <<"   ";} cout << card_collection(card_4.card) << "║";
-    cout << "\n╚═════╝" << "  " << "╚═════╝" << "  " << "╚═════╝" << "  " << "╚═════╝";
+    if(card_4.card == 10){cout <<"  ";} else{cout <<"   ";} cout << card_collection(card_4.card) << "║  ║" << base_cards[card_5.suits % 4];
+    if(card_5.card == 10){cout <<"  ";} else{cout <<"   ";} cout << card_collection(card_5.card) << "║";
+
+    cout << "\n╚═════╝" << "  " << "╚═════╝" << "  " << "╚═════╝" << "  " << "╚═════╝" << "  " << "╚═════╝";
 }
 // ╔═════╗
 // ║A   ♠║ print_C(14,0)
